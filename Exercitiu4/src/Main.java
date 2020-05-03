43e0de06e2f747ece8ad05db77ea922e23061743
Stan Silvia <Silviastan2902@yahoo.com>
2020-05-03 12:04:13 +0300

    Am terminat Ex4

--- Exercitiu4/src/Main.java
+++ Exercitiu4/src/Main.java
@@ -5,4 +5,30 @@ public class Main {
 
     //Matei
 
+
+    public static void main(String[] args){
+
+        String text = "Obiectul istoriei nu se limiteaza doar la evenimentele politice, cum ar fi de exemplu bataliile sau faptele";
+
+
+        int numarCuvinte = numereCuvinte(text);
+        System.out.println("Numarul de cuvinte este " + numarCuvinte);
+    }
+
+
+
+    public static int numereCuvinte(String text){
+
+        int numarCuvinte=0;
+        String[] cuvinte = text.split(" ");
+        for(int cursor = 0; cursor < cuvinte.length; cursor++){
+            if(cuvinte[cursor].length() >2 && cuvinte[cursor].contains("i") && cuvinte[cursor].indexOf("i") % 2 == 0){
+                numarCuvinte++;
+            }
+
+        }
+
+        return numarCuvinte;
+
+    }
 }
