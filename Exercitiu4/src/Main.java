d667d0d9364387282046e9f470425adbb4378b53
Stan Silvia <Silviastan2902@yahoo.com>
2020-05-03 11:46:45 +0300

    Ex4 neterminat

---
+++ Exercitiu4/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    //Se da un text:
+    //Sa se numere toate cuvintele care au lungimea mare decar 2, contin caracterul "i" si caract "i" se afla pe o pozitie para
+
+    //Matei
+
+}
