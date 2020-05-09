878570400b2a30340b62066a4e0e0c36fded621b
Stan Silvia <Silviastan2902@yahoo.com>
2020-05-09 10:17:27 +0300

    exercitiu calculator

---
+++ src/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+    //Acesta este un constructor default, il folosim ca sa instantiem obiecte
+    public Calculator(){
+
+
+    }
+
+
+    public int adunare(int a, int b){
+
+        return a + b;
+
+    }
+
+    public int scadere(int a, int b){
+        return a - b;
+
+    }
+
+    public int inmultire(int a, int b){
+
+        return a * b ;
+
+    }
+    public int impartire( int a, int b){
+        return a / b;
+    }
+
+}
